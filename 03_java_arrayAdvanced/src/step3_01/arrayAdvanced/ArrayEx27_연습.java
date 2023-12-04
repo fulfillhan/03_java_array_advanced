d608e72326d8a32b450e05d64c3dce6b852cca51
sanghui <tkdgml719@gamil.com>
2023-12-04 13:45:42 +0900

    2023-12-04 오전커밋

--- 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx27_연습.java
+++ 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx27_연습.java
@@ -1,5 +1,5 @@
 package step3_01.arrayAdvanced;
-//
+//2023-12-03 오전
 /*
  * # 숫자이동[2단계]
  * 
@@ -11,11 +11,75 @@ package step3_01.arrayAdvanced;
  * 5. 단, 숫자3을 입력하면, 벽을 격파할 수 있다.
  */
 
+import java.util.Scanner;
+
 public class ArrayEx27_연습 {
 	
 public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-		int[] game = {0,0,1,0,2,0,0,1,0};
+		int[] game = {0,0,1,0,2,0,0,1,0,0,1,0,0,0,1,0};
+		int playerIdx = 0;
+		
+		//player 위치값 구하기
+		for (int i = 0; i < game.length; i++) {
+			if (game[i] == 2) {    // 2는 캐릭터, 해당 요소와 같아지면 위치값 = player
+				playerIdx = i;
+			}
+		}
+		
+		while (true) {
+			
+			
+			// 화면 출력
+			for (int i = 0; i < game.length; i++) {   //배열 상태를 살펴보고
+				if (game[i] == 2) {                   //요소가 2이면
+					System.out.print("옷 ");          //"옷" 으로 출력한다.
+				}
+				else {
+					System.out.print(game[i]+ " ");   //그렇지 않으면 해당요소 그대로 출력한다.
+				}
+			}
+			System.out.println();
+			
+			System.out.print("왼쪽(1) 오른쪽(2) 종료(0) 숫자입력 : ");
+			int sel = scanner.nextInt();
+			
+			if (sel== 1 ) {
+				if (playerIdx -1 >= 0) {              //범위를 벗어나지 않도록 한다/ 위동하려는 위치가 첫위치0 이상이어야한다.
+					if (game[playerIdx -1] == 1) {    // 이동하려는 위치의 요소에 장애물이 있는지 확인
+						System.out.print("격파[3] 입력 : ");
+						int punch = scanner.nextInt();
+						if (punch != 3) {             //3이 아니라면 다시 루프 시작
+							continue;
+						}
+					}
+					game[playerIdx -1] =2;
+					game[playerIdx] = 0;
+					playerIdx--;
+				}
+			}
+			else if (sel == 2) {
+				if (playerIdx+1 < game.length) {
+					if (game[playerIdx+ 1] == 1) {
+						System.out.print("격파[3] 입력 : ");
+						int punch = scanner.nextInt();
+						if (punch != 3) {
+							continue;
+						}
+					}
+					game[playerIdx + 1] = 2;
+					game[playerIdx] = 0;
+					playerIdx++;
+				}
+			}
+			else if (sel== 0 ) {
+				System.out.println("==프로그램 종료==");
+				scanner.close();                              
+				break;
+			}
+			  
+		}
 		
 	}
 
