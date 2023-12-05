fc30c12cc5ee320c3470212cead421fa64604fb5
sanghui <tkdgml719@gamil.com>
2023-12-05 14:17:09 +0900

    2023-12-05 오전 커밋

--- 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx28_연습.java
+++ 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx28_연습.java
@@ -1,8 +1,10 @@
 package step3_01.arrayAdvanced;
 
+
 import java.util.Random;
 
-//2023-12-04 오전
+
+//2023-12-04 
 
 /*
  * # 중복숫자 금지[2단계]
@@ -16,24 +18,36 @@ public class ArrayEx28_연습 {
 public static void main(String[] args) {
 		Random ran = new Random();
 		int[] arr = new int[5];
+		int i = 0;
 		
-		int cnt = 0; //반복 횟수
-		
-		while (cnt < 5) {
-		int rNum = ran.nextInt(10)+1;
-		
-		int dupleCnt = 1;
-		for (int i = 0; i < arr.length; i++) {
-			 if (arr[i] == rNum) {
-				dupleCnt ++;
+		while(i< arr.length) {
+			int rNum = ran.nextInt(10)+1;
+			int dupleCheck = 1; // 중복이면 -1, 중복아니면 1
+			
+			//반복문으로 중복 확인 절차
+			for (int j = 0; j < arr.length; j++) {
+				if (rNum == arr[j]) {
+					dupleCheck = -1;
+					break; //  중복이 확인되면 해당문을 반복하지 않고, 바깥쪽 반복문으로 이동
+				}
 			}
-				
+			
+			if (dupleCheck ==1) {
+				arr[i] = rNum;
+				i++;
 			}
 		}
 		
-			
+		for (int j = 0; j < arr.length; j++) {
+			System.out.print(arr[j]+ " ");
 		}
 	}
+	
+}
+		
+			
+
+
 	
 
 
