d703f0a8d5d9de6f9c7beb03d1fbe35efbe54e14
sanghui <tkdgml719@gamil.com>
2024-01-15 12:21:33 +0900

    2024-01-15 오전 커밋

--- 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx43_연습2.java
+++ 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx43_연습2.java
@@ -2,25 +2,25 @@ package step3_01.arrayAdvanced;
 //2024-01-10
 /*
  * # 달팽이
- * 
- * 1	2	3	4	5	
- * 16	17	18	19	6	
- * 15	24	25	20	7	
- * 14	23	22	21	8	
+ *
+ * 1	2	3	4	5
+ * 16	17	18	19	6
+ * 15	24	25	20	7
+ * 14	23	22	21	8
  * 13	12	11	10	9
- * 
- * 
+ *
+ *
  * 1) 오른쪽 방향으로 순서대로 숫자저장
  * 2) 아래쪽 방향으로 순서대로 숫자저장
  * 3) 왼쪽 방향으로 순서대로 숫자저장
  * 4) 윗쪽 방향으로 순서대로 숫자저장
- * 
+ *
  * 1)2)3)4) 반복
  */
 
 
 public class ArrayEx43_연습2 {
-
+	
 	public static void main(String[] args) {
 		
 		int size = 5;
@@ -35,6 +35,45 @@ public class ArrayEx43_연습2 {
 		map[y][x] = num;
 		isUsed[y][x] = true;
 		
+		for (int i = 0; i < 24; i++) {
+			 int xx = x; //변화하는 행의 변수
+			 int yy = y; //변화하는 열의 변수
+			 
+				
+			 if(dir == 1)        xx++;  //오른쪽 이동
+			 else if (dir == 2)  yy++;  //아래로 이동
+			 else if (dir == 3)  xx--;  //왼쪽으로 이동
+			 else if (dir == 4)  yy--;  //위로 이동
+				
+			//방향이 달라지는 로직
+			if(xx < 0 || size <= xx || yy < 0 || yy >= size || isUsed[yy][xx] == true){
+				dir ++;
+				xx = x;
+				yy = y;
+				
+				if(dir == 5) dir =1;
+				
+				if(dir == 1)        xx++;
+				else if (dir == 2)  yy++;
+				else if (dir == 3)  xx--;
+				else if (dir == 4)  yy--;
+			}
+			num ++;
+			x = xx;
+			y = yy;
+			
+			map[y][x] = num;
+			isUsed[y][x] = true;
+			
+		}
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				System.out.print(map[i][j]+ "\t");
+			}
+			System.out.println();
+		}
+		
 	}
-
+	
 }
