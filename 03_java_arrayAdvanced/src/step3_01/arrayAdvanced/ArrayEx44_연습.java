9732b1c9fb338f83c0fe5ba39add4df81a49e689
sanghui <tkdgml719@gamil.com>
2024-03-30 22:50:33 +0900

    재커밋

---
+++ 03_java_arrayAdvanced/src/step3_01/arrayAdvanced/ArrayEx44_연습.java
@@ -0,0 +1,37 @@
+package step3_01.arrayAdvanced;
+
+import java.util.Scanner;
+
+// #오목
+
+public class ArrayEx44_연습 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int size = 10;
+		int[][] omok = {
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0}
+		};
+
+		int p1Y = 0;
+		int p1X = 0;
+		int p2Y = 0;
+		int p2X = 0;
+		int turn = 0;
+		int win = 0;
+		
+		
+	}
+
+}
